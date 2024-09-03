4ca3c0b82eb2e0f5d0b527ef76b2a42b8d8d7dea
maxcol01 <maxcollet01@gmail.com>
2024-09-03 13:01:44 +0200

    updated files

--- write.py
+++ write.py
@@ -1,3 +1,25 @@
-poem = "oulalalalala"
+poem = [("Roses", "red"), ("Violet", "blue"),
+        ("Sugar", "sweet")
+        ]
+
+text = ""
+for item in poem:
+    if item[0] == "Sugar":
+        text += f"{item[0]} is {item[1]}\n"
+    else:
+        text += f"{item[0]} are {item[1]}\n"
+
+text += "And so are you !\n"
+
+# using write mode will create the file if it does not exist !
+# also: write erase the previous text !
 with open("writeTo.txt", mode="w") as file:
-    file.write(poem)
+    file.write(text)
+#with open("writeTo.txt", mode="w") as file:
+#    file.write("et moi peduuuu") # this erased the poem !
+
+# to add to an existing file: .wite() but in append mode !
+
+with open("writeTo.txt", mode="a") as file:
+    file.write("et moi peduuuu \n")
+    file.writelines(["hihih\n", "hahaha\n"])

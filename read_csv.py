d6c9283df6944a6fd7be5d6c8858aec7f527b9ff
maxcol01 <maxcollet01@gmail.com>
2024-09-04 08:33:57 +0200

    add a .gitignore file and modification of read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,2 +1,10 @@
 import csv
 
+with open("toy_data.csv", mode="r") as file:
+    # access the information inside a .csv file
+    content = csv.reader(file)
+    # skip the header
+    next(content)
+    # loop over the row of the csv (return a list of each element within a column
+    for row in content:
+        print(row)

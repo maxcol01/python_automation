80773a26561ca5331442dc7a142ecb0272bb1a2d
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-06 13:45:57 +0200

    modification example1.py

--- section4/example1.py
+++ section4/example1.py
@@ -1,3 +1,4 @@
+#  this is a relative path. If we want the absolute path, we need to reference the root of our systm
 path = "../../section3/pythonProject/"
 
 with open(path+"jokes.txt", mode="r") as file:

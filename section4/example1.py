9c52c8020035830b8cfae9bd4fd26d6ef3cfccb5
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-08 19:13:52 +0200

    modification of typos

--- section4/example1.py
+++ section4/example1.py
@@ -1,10 +1,9 @@
 #  this is a relative path. If we want the absolute path, we need to reference the root of our systm
 path = "../../section3/pythonProject/"
 
-with open(path+"jokes.txt", mode="r") as file:
+with open(path + "jokes.txt", mode="r") as file:
     content = file.read()
     print(content)
-
 
 #  the problem here is the fact that depending on the OS we may have different way
 #  to write the path ! so we need to use Pathlib !

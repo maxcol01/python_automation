81395c60b33bb5c19c4caf839f188ee834e22188
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-09 13:08:02 +0200

    creating a new file

---
+++ section4/iterating_over_files.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+#  What if we need to deal with several path to our files ?
+# python embeds the iterdir() method -> generate an iterator of the files and folder in
+# another folder allowing to iterate over !
+

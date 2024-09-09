83169da6a8e43abd0a3589248873878496f2b077
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-09 13:12:51 +0200

    modification to the files

--- section4/iterating_over_files.py
+++ section4/iterating_over_files.py
@@ -3,3 +3,10 @@ from pathlib import Path
 # python embeds the iterdir() method -> generate an iterator of the files and folder in
 # another folder allowing to iterate over !
 
+# Path object of the directory  we want to iterate over
+
+path = Path.home() / 'to_the_folder'
+
+for item in path.iterdir():
+    # this provides us a path to every files in our folder !
+    print(item)

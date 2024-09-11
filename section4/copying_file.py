a9e69063a031c666deccd8424e11872abc3b80ca
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-11 21:20:00 +0200

    new commit

--- section4/copying_file.py
+++ section4/copying_file.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+import shutil as sh
 
-path_to_file = ("/Users/maximecollet/Desktop/Bureau - iMac de Maxime/DOC "
-                "MAX/Courses/ZTM_AUTOMATIZATION_BOOTCAMP/section4")
+src = Path.home() / "Desktop" / "dad_jokes.csv"
 
 folder_path = Path(".") / "python_automation" / "copying_stuff"
 folder_path.mkdir(exist_ok=True)
+
+#  to copy we need to specify a source path and a destination path !
+sh.copy(src, folder_path)
+

e79f0d46c0ec5abec12eb3d5c01de0a244d61c10
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-11 21:13:47 +0200

    new file

---
+++ section4/copying_file.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+path_to_file = ("/Users/maximecollet/Desktop/Bureau - iMac de Maxime/DOC "
+                "MAX/Courses/ZTM_AUTOMATIZATION_BOOTCAMP/section4")
+
+folder_path = Path(".") / "python_automation" / "copying_stuff"
+folder_path.mkdir(exist_ok=True)

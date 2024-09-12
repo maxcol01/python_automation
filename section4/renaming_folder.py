e12881b12cca35a991100c9523100f2993310aec
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-12 08:34:34 +0200

    new file commited

--- section4/renaming_folder.py
+++ section4/renaming_folder.py
@@ -1,4 +1,4 @@
-from pathlib import  Path
+from pathlib import Path
 import shutil as sh
 
 common_path = Path(".") / "python_automation" / "copying_stuff" / "moving_folder"
@@ -7,3 +7,12 @@ dest_folder = common_path / "renamed_folder"
 
 if src_folder.exists():
     sh.move(src=src_folder, dst=dest_folder)
+
+# what if we want to copy and renaming at the same time ?
+
+common_path_file = Path(".") / "python_automation" / "copying_stuff" / "moving_folder"
+src_file = common_path_file / "bad_jokes.csv"
+dest_file = common_path_file / "renamed_folder" / "sad_jokes.csv"
+
+if src_file.exists():
+    sh.move(src=src_file, dst=dest_file)

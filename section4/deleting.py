1afbe1ddd18f7ebe7e17c66fa460a19dada35ee5
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-09-12 09:01:15 +0200

    deleting file

--- section4/deleting.py
+++ section4/deleting.py
@@ -4,7 +4,9 @@ import shutil as sh
 # Create a folder to store our toy files
 
 folder_path = pt.Path(".") / "toy_folder"
+new_folder_path = folder_path / "deleted_folder"
 folder_path.mkdir(exist_ok=True, parents=True)
+new_folder_path.mkdir(exist_ok=True, parents=True)
 
 # Moving our toy files to the toy_folder:
 dest_file = folder_path
@@ -12,3 +14,18 @@ for i in range(1, 4):
     src_file = pt.Path(".") / f"test{i}.txt"
     if src_file.exists():
         sh.move(src=src_file, dst=dest_file)
+
+# delete file: use .unlink() method
+src_file_delete = folder_path / "test1.txt"
+src_file_delete.unlink(missing_ok=True)
+
+# delete folder: use .rmdir() (directory empty)
+src_folder_delete = new_folder_path
+if src_folder_delete.exists():
+    src_folder_delete.rmdir()
+
+# What if the folder is not empty ? .rmtree() -> shutil module.
+src_folder_delete_new = folder_path
+if src_folder_delete_new.exists():
+    sh.rmtree(src_folder_delete_new)
+
